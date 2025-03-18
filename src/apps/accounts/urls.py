81a34265d28e93f70d113304cd59d7824a89d8ab
Sankalp Pimpalkar <pimpalkarsankalp8@gmail.com>
2025-03-18 13:56:40 +0530

    added generate api key view with model

--- src/apps/accounts/urls.py
+++ src/apps/accounts/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('register/', register),
     path('login/', login),
     path('logout/', logout),
+    path('get-user/', get_user_details),
+    path('refresh-token/', refresh_access_token),
 ]
